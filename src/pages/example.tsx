4bc9e8e9624ff3ffd37134bfd944fe887a9b0044
NFQ Hung Nguyen <phihung.nguyen@nfq.asia>
2023-03-03 18:34:24 +0700

    MAKAIRA-3311 [Component] Select

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -1,6 +1,6 @@
 import { FaArrowLeft } from 'react-icons/fa'
 
-import { PageWrapper, Link, Button } from '@/components'
+import { PageWrapper, Link, Button, Select } from '@/components'
 import { withMakaira } from '@/makaira/withMakaira'
 
 export default function Example() {
@@ -11,6 +11,113 @@ export default function Example() {
           Back to homepage
         </Button>
       </Link>
+
+      <Select
+        description="The selected type will be used when comparing two products."
+        title="Default select"
+        allowClear={true}
+        showSearch={true}
+        placeholder="Choose an importer..."
+        options={[
+          { value: 'equals', label: 'equals' },
+          { value: 'greater', label: 'greater' },
+          { value: 'less', label: 'less' },
+          { value: 'between', label: 'between' },
+        ]}
+      />
+
+      <Select
+        description="The selected type will be used when comparing two products."
+        title="Large select"
+        allowClear={true}
+        showSearch={true}
+        size="large"
+        placeholder="Choose an importer..."
+        options={[
+          { value: 'equals', label: 'equals' },
+          { value: 'greater', label: 'greater' },
+          { value: 'less', label: 'less' },
+          { value: 'between', label: 'between' },
+        ]}
+      />
+
+      <Select
+        description="The selected type will be used when comparing two products."
+        title="Error select"
+        error={true}
+        placeholder="Choose an importer..."
+        options={[
+          { value: 'equals', label: 'equals' },
+          { value: 'greater', label: 'greater' },
+        ]}
+      />
+
+      <Select
+        description="The selected type will be used when comparing two products."
+        title="Grouped options select"
+        allowClear={true}
+        showSearch={true}
+        placeholder="Choose an importer..."
+        groupOptions={[
+          {
+            label: '',
+            children: [
+              {
+                value: '',
+                label: 'Alle Importer',
+              },
+            ],
+          },
+          {
+            label: 'Laufend',
+            children: [
+              {
+                value: 'stage--oxid6--1-0',
+                label: 'live - en, de',
+              },
+            ],
+          },
+          {
+            label: 'Beendet',
+            children: [
+              {
+                value: 'stage--oxid6--2-gc8j5',
+                label: 'manuell - vor 6 Monaten',
+              },
+              {
+                value: 'stage--oxid6--2-smxh5',
+                label: 'manuell - vor 6 Monaten',
+              },
+              {
+                value: 'stage--oxid6--2-vb4m2',
+                label: 'manuell - vor 6 Monaten',
+              },
+              {
+                value: 'stage--oxid6--2-x2gpl',
+                label: 'manuell - vor 6 Monaten',
+              },
+              {
+                value: 'stage--oxid6--2-p5qhk',
+                label: 'manuell - vor 6 Monaten',
+              },
+            ],
+          },
+        ]}
+      />
+
+      <Select
+        description="The selected type will be used when comparing two products."
+        title="Disabled select"
+        disabled
+        placeholder="Choose an importer..."
+      />
+
+      <Select
+        description="The selected type will be used when comparing two products."
+        title="Empty/Borderless select"
+        placeholder="Choose an importer..."
+        borderless={true}
+      />
     </PageWrapper>
   )
 }
