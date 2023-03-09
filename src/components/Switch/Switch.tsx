31d5d8a138230ca34ebe4371d4b80c6e59c08494
Dat Lieu <dat.lieu@nfq.asia>
2023-03-09 11:54:41 +0700

    fix: remove unused prop, update style

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,6 +1,6 @@
-﻿import { cloneElement, FC } from 'react';
+﻿import { FC } from 'react';
 import classNames from 'classnames'
-import RcSwitch, { SwitchChangeEventHandler, SwitchClickEventHandler } from 'rc-switch'
+import RcSwitch, { SwitchChangeEventHandler } from 'rc-switch'
 import styles from '@/components/Switch/Switch.module.scss'
 import "rc-switch/assets/index.css"
 
@@ -9,22 +9,12 @@ export type SwitchProps = {
   defaultChecked?: boolean;
   checked?: boolean;
   disabled?: boolean;
-  checkedChildren?: React.ReactNode;
-  unCheckedChildren?: React.ReactNode;
   onChange?: SwitchChangeEventHandler;
-  onKeyDown?: React.KeyboardEventHandler<HTMLButtonElement>;
-  onClick?: SwitchClickEventHandler;
-  tabIndex?: number;
   /**
    * Default: vertical
    */
   type?: 'vertical' | 'horizontal',
   className?: string;
-  labelClassName?: string;
-  loading?: boolean;
-  loadingIcon?: React.ReactElement;
-  style?: React.CSSProperties;
-  size?: 'small' | 'large';
 }
 
 const Switch: FC<SwitchProps> = ({
@@ -32,44 +22,22 @@ const Switch: FC<SwitchProps> = ({
   defaultChecked,
   checked,
   disabled,
-  checkedChildren,
-  unCheckedChildren,
   onChange,
-  onKeyDown,
-  onClick,
-  tabIndex,
   type = 'vertical',
   className,
-  labelClassName,
-  loadingIcon,
-  style,
-  size = 'large',
-  loading = false,
 }) => {
 
-  let LoadingIcon = null;
-  if (loading && loadingIcon) {
-    LoadingIcon = cloneElement(loadingIcon, { className: 'switch--loading' })
-  }
-  
   return (
     <div
-      className={classNames(styles['switch-input'], styles[`switch-input--${type}`], styles[size], className)}
+      className={classNames(styles['switch-input'], styles[`switch-input--${type}`], className)}
     >
-      {title && <div className={classNames(styles["switch-input__label"], labelClassName)}>{title}</div>}
+      {title && <div className={styles["switch-input__label"]}>{title}</div>}
       <RcSwitch
         title={title}
         checked={checked}
         defaultChecked={defaultChecked}
-        checkedChildren={checkedChildren}
-        unCheckedChildren={unCheckedChildren}
         onChange={onChange}
-        onKeyDown={onKeyDown}
-        onClick={onClick}
-        tabIndex={tabIndex}
-        disabled={disabled || loading}
-        style={style}
-        loadingIcon={LoadingIcon}
+        disabled={disabled}
       />
     </div>
   )
