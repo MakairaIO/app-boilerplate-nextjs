aa3ce11418c1f9092875fc9cd45e5cd5b897ce4c
Dat Lieu <dat.lieu@nfq.asia>
2023-03-09 14:48:37 +0700

    MAKAIRA-3320 [Component] Update Buttons

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -6,11 +6,44 @@ import { withMakaira } from '@/makaira/withMakaira'
 export default function Example() {
   return (
     <PageWrapper title="Example page" prefix="You are looking at">
-      <Link pathname="/">
-        <Button icon={FaArrowLeft} variant="primary" iconPosition="left">
+      <div>
+        <Link pathname="/">
+          <Button icon={FaArrowLeft} variant="primary" iconPosition="left" loading>
+            Back to homepage
+          </Button>
+        </Link>
+        <Button icon={FaArrowLeft} variant="secondary" iconPosition="right">
           Back to homepage
         </Button>
-      </Link>
+        <Button icon={FaArrowLeft} variant="reduced" iconPosition="left">
+          Back to homepage
+        </Button>
+        <Button icon={FaArrowLeft} variant="primary" iconPosition="right">
+          Button icon right
+        </Button>
+        <Button icon={FaArrowLeft} variant="primary" iconPosition="left" loading>
+          Button loading
+        </Button>
+        <Button icon={FaArrowLeft} variant="primary" iconPosition="left" disabled>
+          Button Disabled
+        </Button>
+        <Button icon={FaArrowLeft} variant="primary" iconPosition="left" level={-1}>
+          Button level -1
+        </Button>
+        <Button icon={FaArrowLeft} variant="primary" iconPosition="left" level={0}>
+          Button level 0
+        </Button>
+        <Button icon={FaArrowLeft} variant="primary" iconPosition="left" level={1}>
+          Button level 1
+        </Button>
+        <Button icon={FaArrowLeft} variant="primary" iconPosition="left" />
+        <Button icon={FaArrowLeft} variant="primary" iconPosition="left" loading />
+        <Button icon={FaArrowLeft} variant="primary" iconPosition="left" disabled />
+
+        <Button icon={FaArrowLeft} variant="primary" iconPosition="left" tooltip='An additional info tooltip is displayed'>
+          Button with tooltip
+        </Button>
+      </div>
       <br />
       <br />
       <Tooltip
