3df13ad1511e9f89e89c832fe4a6b7cb8545753f
Dat Lieu <dat.lieu@nfq.asia>
2023-03-10 15:23:13 +0700

    feat: number input component

--- src/pages/example.tsx
+++ src/pages/example.tsx
@@ -2,8 +2,11 @@ import { FaArrowLeft } from 'react-icons/fa'
 
 import { PageWrapper, Link, Button } from '@/components'
 import { withMakaira } from '@/makaira/withMakaira'
+import { useState } from 'react'
+import NumberInput from '@/components/NumberInput/NumberInput'
 
 export default function Example() {
+  const [number, setNumber] = useState(10)
   return (
     <PageWrapper title="Example page" prefix="You are looking at">
       <Link pathname="/">
@@ -11,6 +14,8 @@ export default function Example() {
           Back to homepage
         </Button>
       </Link>
+      <NumberInput value={number} max={20} min={5} label="Number Input" description='Input description' onChange={setNumber}/>
+      <NumberInput value={number} max={20} min={5} label="Number Input Disabled" disabled/>
     </PageWrapper>
   )
 }
