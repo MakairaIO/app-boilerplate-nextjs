a713066e759d0d359cf66e13e40496280bb2d5bf
ThuanLe <thuan.le@marmalade.group>
2023-03-13 15:27:48 +0700

    feat(MAKAIRA-3363): add modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,50 @@
+import csx from 'classnames'
+import React, { FunctionComponent, ReactNode } from 'react'
+import Dialog from 'rc-dialog'
+import {IDialogPropTypes} from 'rc-dialog/lib/IDialogPropTypes'
+import { FaTimes } from 'react-icons/fa'
+
+import 'rc-dialog/assets/index.css'
+import styles from '@/components/Modal/Modal.module.scss'
+
+
+export interface ModalProps extends IDialogPropTypes {
+  header?: string | ReactNode,
+}
+
+const Modal: FunctionComponent<ModalProps> = ({
+  header = '',
+  children,
+  footer = null,
+  onClose = () => {},
+  className = '',
+  mask = false,
+  ...rest
+}) => {
+  if (!rest?.visible) return <></>
+
+  return (
+    <Dialog
+      style={{...rest?.style}}
+      closeIcon={<FaTimes/>}
+      className = {
+        csx(`${styles.modal} ${className}`)
+      }
+      title={header}
+      onClose={onClose}
+      mask={mask}
+      animation="fade"
+      maskAnimation="fade"
+      footer={footer && <div className={styles.modalFooter}>{footer}</div>}
+      destroyOnClose
+      wrapClassName={`${styles.wrapper}`}
+      {...rest}
+    >
+      {children && (
+        <div className={styles.modalContent}>{children}</div>
+      )}
+    </Dialog>
+  )
+}
+
+export { Modal }
