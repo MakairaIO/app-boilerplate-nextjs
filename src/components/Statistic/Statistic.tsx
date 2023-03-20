2e6717edab78ba7f606d8032305f8a70cb73a258
NFQ Hung Nguyen <phihung.nguyen@nfq.asia>
2023-03-20 14:03:33 +0700

    MAKAIRA-3371 Add statistic component

---
+++ src/components/Statistic/Statistic.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import styles from '@/components/Statistic/Statistic.module.scss'
+import { Text } from '../Text/Text'
+import csx from 'classnames'
+import { FaCaretDown, FaCaretUp } from 'react-icons/fa'
+import { Tooltip } from '../Tooltip/Tooltip'
+
+interface StatisticProps {
+  value: string
+  title: string
+  text: string
+  tooltip?: string
+  type?: 'primary' | 'secondary'
+  status?: 'up' | 'down'
+  onClick?: () => void
+}
+
+const Statistic: React.FC<StatisticProps> = ({
+  value,
+  type = 'primary',
+  title,
+  text,
+  status,
+  tooltip,
+  onClick,
+}) => {
+  const getCaret = () => {
+    return !status ? (
+      <></>
+    ) : status === 'up' ? (
+      <FaCaretUp className={styles.caretUp} />
+    ) : (
+      <FaCaretDown className={styles.caretDown} />
+    )
+  }
+
+  return (
+    <div
+      className={csx(styles.wrapper, styles[type])}
+      onClick={onClick}
+      style={onClick ? { cursor: 'pointer' } : {}}
+    >
+      <div className={styles.value}>
+        <Text element="span" size="echo" weight="medium">
+          {value}
+        </Text>
+        {getCaret()}
+      </div>
+      <Text
+        className={styles.title}
+        element="span"
+        size="charlie"
+        weight="medium"
+      >
+        {title}
+      </Text>
+      {tooltip ? (
+        <Tooltip placement="top" overlay={tooltip}>
+          <Text
+            className={styles.text}
+            element="span"
+            size="bravo"
+            weight="medium"
+          >
+            {text}
+          </Text>
+        </Tooltip>
+      ) : (
+        <Text
+          className={styles.text}
+          element="span"
+          size="bravo"
+          weight="medium"
+        >
+          {text}
+        </Text>
+      )}
+    </div>
+  )
+}
+
+Statistic.displayName = 'Statistic'
+export { Statistic }
